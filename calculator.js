44325deff26d88e186e12302b3c41931fe804693
marjika <bluegoo5@hotmail.com>
2018-10-23 14:48:55 -0600

    initial code for simple calulator

---
+++ calculator.js
@@ -0,0 +1,22 @@
+function Calculator() {
+    this.total = 0;
+}
+
+Calculator.prototype.add = function(num) {
+    return this.total += num;
+}
+
+Calculator.prototype.subtract = function(num) {
+    return this.total -= num;
+}
+
+Calculator.prototype.multiply = function(num) {
+    return this.total *= num;
+}
+
+Calculator.prototype.divide = function(num) {
+    if (num === 0) {
+        throw new Error("Cannot divide by zero");
+    }
+    return this.total /= num;
+}
